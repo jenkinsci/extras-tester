da867e65dcdc8bfcdf2bc074102c4d1f1c42d950
jbq <jbq@@java.net>
2007-09-20 09:53:10 +0000

    Resize the thread pool also when setPollingThreadCount() is *not* called during load()

--- src/test/java/hudson/model/SubversionPollingTest.java
+++ src/test/java/hudson/model/SubversionPollingTest.java
@@ -9,7 +9,6 @@ import hudson.triggers.Trigger;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
-import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.GregorianCalendar;
 import java.util.logging.Logger;
